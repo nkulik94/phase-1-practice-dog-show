c58afbfb6b5dc78d6de2c834053589315713bc55
Naftali Kulik <nkulik1994@gmail.com>
2022-05-12 10:02:59 -0400

    built functions to handle request and render response

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    getDogs()
 })
+
+function getDogs() {
+    fetch('http://localhost:3000/dogs')
+    .then(res => res.json())
+    .then(dogs => dogs.map(dog => renderDog(dog)))
+}
+
+function renderDog(dog) {
+    const tr = document.createElement('tr')
+    function makeTd(text) {
+        const td = document.createElement('td')
+        td.textContent = text
+        return tr.appendChild(td)
+    }
+    makeTd(dog.name)
+    makeTd(dog.breed)
+    makeTd(dog.sex)
+    const button = document.createElement('button')
+    button.textContent = 'Edit'
+    makeTd('').appendChild(button)
+    document.querySelector('table').appendChild(tr)
+}
